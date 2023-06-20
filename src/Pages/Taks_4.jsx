8230b4442a06a58795edfa7a833f7a85f1e83257
Jamaluddin Al-Afgani <alafgan23@gmail.com>
2023-06-20 12:38:17 +0800

    update heatmap (titik fokus)

--- src/Pages/Taks_4.jsx
+++ src/Pages/Taks_4.jsx
@@ -16,18 +16,18 @@ export default function Taks_4( ) {
 
 
 
-  useEffect(() => {
-      canvasRef.current.height = document.documentElement.scrollHeight - window.innerHeight;
-      canvasRef.current.width = window.innerWidth;
-      canvasRef.current.height = window.innerHeight;
+  // useEffect(() => {
+  //     canvasRef.current.height = document.documentElement.scrollHeight - window.innerHeight;
+  //     canvasRef.current.width = window.innerWidth;
+  //     canvasRef.current.height = window.innerHeight;
 
-    // const body = document.querySelector('body');
+  //   // const body = document.querySelector('body');
     
-    // canvasRef.current.heig
+  //   // canvasRef.current.heig
  
-    // console.log(window)
-    // console.log(canvasRef.current.height)
-  },[])
+  //   // console.log(window)
+  //   // console.log(canvasRef.current.height)
+  // },[])
 
   
   useEffect(() => {
